6f8383458b4b8f80be7bec9382ad2e2fc3179d3b
Max Lange <max@langes.us>
2023-04-18 16:04:26 -0400

    App component structure layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import ArticleGrid from '../ArticleGrid/ArticleGrid';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+
       </header>
-    </div>
+      <Switch>
+        <Route exact path="/" component={ArticleGrid}/>
+        <Route exact path="/:filters" render={({match}) => (
+            <ArticleGrid filters={match.params.filters} />
+          )}
+        />
+        <Route exact path='/error' render={() => (
+            <h3>Something went wrong!</h3>
+          )} 
+        />
+      </Switch>
+    </>
   );
 }
 
