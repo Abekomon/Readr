6424a00aecfaee2d957ec9fdeed3e950d7987ade
Max Lange <max@langes.us>
2023-04-18 16:27:35 -0400

    Add filter categories  from api documentation

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,8 @@
 import React from "react";
 
 export default function Nav() {
-  
+  const categories = ["home", "arts", "automobiles", "books", "business", "fashion", "food", "health", "insider", "magazine", "movies", "opinion", "politics", "science", "sports", "technology", "theater", "travel", "upshot", "us", "world"]
+
+
+
 }
