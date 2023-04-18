51d1a381fee1c7d2405cd2529747e1996cbd672c
Max Lange <max@langes.us>
2023-04-18 16:04:07 -0400

    Basic component setup

---
+++ src/components/ArticleGrid/ArticleGrid.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../Nav/Nav";
+
+export default function ArticleGrid() {
+  
+  
+  return (
+    <div className="container">
+      <Nav />
+      <div className="article-container">
+        <h2>Hi!</h2>
+      </div>
+    </div>
+  )
+}
