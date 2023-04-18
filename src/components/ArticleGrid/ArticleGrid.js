ce32b8aaafad2356223c3bca9268f610b15219b2
Max Lange <max@langes.us>
2023-04-18 16:26:42 -0400

    Add api call function to a hook, and call on component render

--- src/components/ArticleGrid/ArticleGrid.js
+++ src/components/ArticleGrid/ArticleGrid.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Nav from "../Nav/Nav";
+import { useState, useEffect } from "react";
+import fetchArticles from "../../apiCalls";
 
-export default function ArticleGrid() {
+export default function ArticleGrid({filters}) {
+  const [allArticles, setAllArticles] = useState([])
   
+  useEffect(() => {
+    fetchArticles(filters).then(data => setAllArticles(data.results)) 
+  }, [])
   
+  console.log(allArticles)
+
   return (
     <div className="container">
       <Nav />
