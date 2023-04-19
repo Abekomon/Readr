99b3ec715e102f466609bc6859f0b5c57d11ac83
Max Lange <max@langes.us>
2023-04-18 22:40:39 -0400

    Remove state and hooks from ArticleGrid component

--- src/components/ArticleGrid/ArticleGrid.js
+++ src/components/ArticleGrid/ArticleGrid.js
@@ -1,23 +1,10 @@
 import React from "react";
-import Nav from "../Nav/Nav";
-import { useState, useEffect } from "react";
-import fetchArticles from "../../apiCalls";
 
 export default function ArticleGrid({filters}) {
-  const [allArticles, setAllArticles] = useState([])
-  
-  useEffect(() => {
-    fetchArticles(filters).then(data => setAllArticles(data.results)) 
-  }, [])
-  
-  console.log(allArticles)
 
   return (
     <div className="container">
-      <Nav />
-      <div className="article-container">
-        <h2>Hi!</h2>
-      </div>
+  
     </div>
   )
 }
