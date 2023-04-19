ddc3cb69b6610869305caccc81b4b0e7266743be
Max Lange <max@langes.us>
2023-04-18 22:39:34 -0400

    Update App component to use one route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,31 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import ArticleGrid from '../ArticleGrid/ArticleGrid';
+import Nav from '../Nav/Nav';
+import fetchArticles from '../../apiCalls';
 
 function App() {
+  const [allArticles, setAllArticles] = useState([])
+  const fetchData = (filter) => {
+    fetchArticles(filter).then(data => setAllArticles(data.results)) 
+  }
+
+  useEffect(() => {
+    fetchData('home')
+  }, [])
+
+  console.log(allArticles)
   return (
     <>
       <header>
 
       </header>
+      <Nav fetchData={fetchData} />
       <Switch>
-        <Route exact path="/" component={ArticleGrid}/>
-        <Route exact path="/:filters" render={({match}) => (
-            <ArticleGrid filters={match.params.filters} />
-          )}
-        />
+        <Route exact path="/" render={() => (
+            <ArticleGrid articles={allArticles} />
+        )}/>
         <Route exact path='/error' render={() => (
             <h3>Something went wrong!</h3>
           )} 
