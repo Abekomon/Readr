905591444c0088a34cd8fb893bbcc671b666aeef
Max Lange <max@langes.us>
2023-04-19 18:22:47 -0400

    Add state to ArticleGrid that stores current article and modal open/close state

--- src/components/ArticleGrid/ArticleGrid.js
+++ src/components/ArticleGrid/ArticleGrid.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./ArticleGrid.css"
 import Article from "../Article/Article";
+import Modal from "../Misc/Modal";
+import { useState } from "react";
 
 export default function ArticleGrid({ title, articles }) {
-  const artList = articles.map((art, i) => <Article key={i} data={art} />)
+  const [isOpen, setIsOpen] = useState(false)
+  const [curArticle, setCurArticle] = useState({})
+  const updateModal = (data, modalState) => {
+    setCurArticle(data)
+    setIsOpen(modalState)
+  }
+
+  const artList = articles.map((art, i) => <Article key={i} data={art} updateModal={updateModal} />)
 
   return (
     <>
+      <Modal
+        isOpen={isOpen}
+        data={curArticle}
+        updateModal={updateModal}
+      />
       <h2>{`${title.toUpperCase()} ARTICLES`}</h2>
       <div className="container">
         {artList}
