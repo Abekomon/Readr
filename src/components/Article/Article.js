f51fdcf80bd605d15cc655d0289cb97785d26a3a
Max Lange <max@langes.us>
2023-04-19 18:22:11 -0400

    Add button to Article component that opens a the modal

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Article.css"
 
-export default function Article({ data }) {
+export default function Article({ data, updateModal}) {
   const { title, short_url, multimedia } = data
   let url = '', altText = 'No image available'
   if(multimedia) { url = multimedia[1].url; altText = title }
   return (
     <div className='card'>
-      <button className='details-button'>See Article Details</button>
+      <button className='details-button' onClick={() => updateModal(data, true)} >See Article Details</button>
       <a className='card-link' href={short_url}>
         <img src={url} alt={altText} />
         <p>{title}</p>
