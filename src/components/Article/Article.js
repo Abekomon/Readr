f15f690036d58f0bc29d8bfdd809eb3900062599
Max Lange <max@langes.us>
2023-04-19 13:56:29 -0400

    Add links to cards, as well as add conditional in case of no image

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -2,10 +2,15 @@ import React from "react";
 import "./Article.css"
 
 export default function Article({ data }) {
-  const { title } = data
+  const { title, short_url, multimedia } = data
+  let url = '', altText = 'No image available'
+  if(multimedia) { url = multimedia[1].url; altText = title }
   return (
-    <div className="card">
-      <p>{title}</p>
-    </div>
+    <a className='card-link' href={short_url}>
+      <div className="card">
+        <img src={url} alt={altText} />
+        <p>{title}</p>
+      </div>
+    </a>
   )
 }
