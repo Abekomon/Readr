934e3a64887091472b6710d4b7629198932b06ec
Max Lange <max@langes.us>
2023-04-18 22:51:35 -0400

    Update Nav component to be stateful and fetch new data when changed

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,29 @@
 import React from "react";
+import Category from "./Category";
+import { useState, useEffect } from "react";
 
-export default function Nav() {
+export default function Nav({fetchData}) {
+  const [curValue, setCurValue] = useState('home')
+  
   const categories = ["home", "arts", "automobiles", "books", "business", "fashion", "food", "health", "insider", "magazine", "movies", "opinion", "politics", "science", "sports", "technology", "theater", "travel", "upshot", "us", "world"]
+  const catList = categories.map(cat => <Category key={cat} name={cat} />)
+  const handleChange = (e) => {
+    setCurValue(e.target.value)
+  }
+
+  useEffect(() => {
+    fetchData(curValue)
+  }, [curValue])
 
 
-
+  return (
+    <nav>
+      <select
+        value={curValue}
+        onChange={(e) => handleChange(e)}
+      >
+        {catList}
+      </select>
+    </nav>
+  )
 }
